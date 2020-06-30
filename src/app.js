46ee8ec947e3f4526450b8c5f52b6d54d64c20aa
Juan Carlos Santa Abreu <jcarlossa120@hotmail.com>
2020-06-30 17:10:49 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const app = express();
 //se le indica a la app cual sera el puerto para el backend
 app.set('port', process.env.PORT || 4000);
-//se le indica a la app el tipo de erramientas que usara
+//se le indica a la app el tipo de herramientas que usara
 app.use(cors());
 app.use(express.json());
 
